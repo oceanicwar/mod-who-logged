dec4dce9bcae3017ab3e2982c250ba2f32249a6f
Pagani Walter <paganiwalter@gmail.com>
2022-05-27 00:11:57 -0300

    Warning message when entering a player

--- src/who_logged.cpp
+++ src/who_logged.cpp
@@ -10,59 +10,57 @@
 class WhoLoggedAnnounce : public PlayerScript
 {
 public:
-        WhoLoggedAnnounce() : PlayerScript("WhoLoggedAnnounce") { }
+    WhoLoggedAnnounce() : PlayerScript("WhoLoggedAnnounce") { }
 
-
-    void OnLogin(Player* p)
+    void OnLogin(Player* player)
     {
-        std::string playerIP = p->GetSession()->GetRemoteAddress();
-        std::string playerName = p->GetName();
-        uint32 pAccountID = p->GetSession()->GetAccountId();
-        uint32 pLevel = p->getLevel();
+        std::string playerIP = player->GetSession()->GetRemoteAddress();
+        std::string playerName = player->GetName();
+        uint32 pAccountID = player->GetSession()->GetAccountId();
+        uint32 pLevel = player->getLevel();
         std::string pClass;
         std::ostringstream message;
-
 
         if (!sConfigMgr->GetBoolDefault("PlayerAnnounce", true))
         {
             return;
         }
 
-        switch (p->getClass())
+        switch (player->getClass())
         {
-        case CLASS_WARLOCK:
-            pClass = "Warlock";
-            break;
-        case CLASS_WARRIOR:
-            pClass = "Warrior";
-            break;
-        case CLASS_MAGE:
-            pClass = "Mage";
-            break;
-        case CLASS_SHAMAN:
-            pClass = "Shaman";
-            break;
-        case CLASS_DEATH_KNIGHT:
-            pClass = "Death Knight";
-            break;
-        case CLASS_DRUID:
-            pClass = "Druid";
-            break;
-        case CLASS_HUNTER:
-            pClass = "Hunter";
-            break;
-        case CLASS_PALADIN:
-            pClass = "Paladin";
-            break;
-        case CLASS_ROGUE:
-            pClass = "Rogue";
-            break;
-        case CLASS_PRIEST:
-            pClass = "Priest";
-            break;
+            case CLASS_WARLOCK:
+                pClass = "Warlock";
+                break;
+            case CLASS_WARRIOR:
+                pClass = "Warrior";
+                break;
+            case CLASS_MAGE:
+                pClass = "Mage";
+                break;
+            case CLASS_SHAMAN:
+                pClass = "Shaman";
+                break;
+            case CLASS_DEATH_KNIGHT:
+                pClass = "Death Knight";
+                break;
+            case CLASS_DRUID:
+                pClass = "Druid";
+                break;
+            case CLASS_HUNTER:
+                pClass = "Hunter";
+                break;
+            case CLASS_PALADIN:
+                pClass = "Paladin";
+                break;
+            case CLASS_ROGUE:
+                pClass = "Rogue";
+                break;
+            case CLASS_PRIEST:
+                pClass = "Priest";
+                break;
         }
 
-        sLog->outString("Player '%s' has logged in : Level '%u' : Class '%s' : IP '%s' : AccountID '%u'", playerName.c_str(), pLevel, pClass.c_str(), playerIP.c_str(), pAccountID);
+        LOG_WARN("module", "Player '{}' has logged in : Level '{}' : Class '{}' : IP '{}' : AccountID '{}'", playerName.c_str(), pLevel, pClass.c_str(), playerIP.c_str(), pAccountID);
     }
 };
 
